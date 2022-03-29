0cf945ce788a24c3b11cddca76b09c7852e3b1da
alohasunny <senneik@gmail.com>
2022-03-29 21:03:20 +1300

    Create rough event component css scaffold

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import EventsList from "../components/EventsList";
+
+function Home() {
+  return (
+    <div>
+      <EventsList />
+    </div>
+  );
+}
+
+export default Home;
