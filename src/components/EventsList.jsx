e2336444158d6abbd38f45491fe46473edfcf358
yukie-m <270141737@yoobeestudent.ac.nz>
2022-03-30 23:22:33 +1300

    tested event page

--- src/components/EventsList.jsx
+++ src/components/EventsList.jsx
@@ -1,12 +1,24 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import EventSingle from "./EventSingle";
+import "../css/listings.scss";
 
 function EventsList() {
+  const [listings, setListings] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:4000/api/view-events").then((response) => {
+      if (response.status === 200 && response.data.length > 0) {
+        setListings(response.data);
+      }
+    });
+  }, []);
   return (
     <div>
-      <EventSingle />
-      <EventSingle />
-      <EventSingle />
+      {listings.map((item, index) => (
+        <EventSingle detailsObj={{ ...item }} key={index} />
+      ))}
     </div>
   );
 }
