8e1005fdb9641ce6a0a5844cd88ef74f1b32c48d
shiori0922 <kame09221831@gmail.com>
2022-04-07 19:07:58 +1200

    header changed

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -10,6 +10,7 @@ import { IoBookmarkOutline } from "react-icons/io5";
 import { AiOutlineHistory } from "react-icons/ai";
 import { AiFillSetting } from "react-icons/ai";
 import { MdLogout } from "react-icons/md";
+import { TiLocationArrow } from "react-icons/ti";
 import Modal from "react-modal";
 
 function Nav() {
@@ -53,6 +54,30 @@ function Nav() {
             />
           </div>
         </div>
+      </div>
+
+      <div className="search-section">
+        <div className="search-by-words">
+              <input type="text" placeholder="Search by category, event name, keywords…"/>
+              <AiOutlineSearch className="icon"/>
+        </div>
+
+        <div className="search-by-location">
+          <input type="text" placeholder="Enter the location"/>
+          <TiLocationArrow className="icon"/>
+        </div>
+      </div>
+
+      <div className="category-slider">
+              <ul className="slider-items">
+                <li>Business</li>
+                <li>Finance</li>
+                <li>Health</li>
+                <li>Technology</li>
+                <li>Life</li>
+                <li>Education</li>
+                <li>International</li>
+              </ul>
       </div>
 
       <Modal
