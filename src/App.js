254d2f2afb9ef89d3fe77a56cdb2e0b072d987a3
alohasunny <senneik@gmail.com>
2022-04-13 21:38:48 +1200

    Remove Dashboard

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Header from "./components/Header";
-import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
