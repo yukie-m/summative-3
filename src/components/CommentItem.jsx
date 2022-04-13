2d5f4ccd4b2837cf82d0d5650d1b67fdd1ca9688
shiori0922 <kame09221831@gmail.com>
2022-04-13 22:28:57 +1200

    comment section css

--- src/components/CommentItem.jsx
+++ src/components/CommentItem.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import "../css/comments.scss";
 import { MdAccountCircle } from "react-icons/md";
 
+
 function CommentItem({ comment }) {
   return (
     <div className="comment-card">
       <div className="">
         <div className="comment-userthumb">
-          <MdAccountCircle size={70} className="icon" />
+          <MdAccountCircle size={50} className="icon" />
         </div>
         <div className="comment-username">{comment.userName}</div>
       </div>
